e6e5a403cd7048a34a6dbad17d428decd74c57b6
Moises Valdez <moisesvaldez075@gmail.com>
2021-01-12 16:58:55 -0800

    added router to the website and bootstrap router as well

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header.component.jsx';
 import Footer from './components/Footer.component.jsx';
 import HomeScreen from './screens/HomeScreen.screen';
+import ProductScreen from './screens/ProductScreen.screen';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main className='py-3'>
                 <Container>
-                    <HomeScreen />
+                    <Route exact path='/' component={HomeScreen} />
+                    <Route path='/product/:id' component={ProductScreen} />
                 </Container>{' '}
             </main>{' '}
             <Footer />
-        </>
+        </Router>
     );
 };
 
