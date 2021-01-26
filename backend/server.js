60b02975c912e3fd5c41953a6da00b4a20f0ad63
Moises Valdez <moisesvaldez075@gmail.com>
2021-01-25 16:28:18 -0800

    created a api server to use with proshop

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
+const products = require('./data/products');
+const PORT = 5000;
 const app = express();
 
-const PORT = 5000;
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(product => product._id === req.params.id);
+
+    res.json(product);
+});
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
