0db6a8685f23b6233114143883fa12f96214990a
Moises Valdez <moisesvaldez075@gmail.com>
2021-01-25 16:16:28 -0800

    added a backend to the proshop

--- frontend/src/screens/ProductScreen.screen.jsx
+++ frontend/src/screens/ProductScreen.screen.jsx
@@ -1,12 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+    Row,
+    Col,
+    Image,
+    ListGroup,
+    ListGroupItem,
+    Card,
+    Button
+} from 'react-bootstrap';
 import Rating from '../components/Rating.component';
 import products from '../products';
 
-const ProductScreen = ({ history, location, match }) => {
+const ProductScreen = ({ match }) => {
     const product = products.find(product => product._id === match.params.id);
 
-    return <>{product.name}</>;
+    return (
+        <>
+            <Link className='btn btn-link my-3' to='/'>
+                Go Back
+            </Link>
+
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                    </ListGroup>
+
+                    <ListGroup>
+                        <ListGroupItem>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews}`}
+                            />
+                        </ListGroupItem>
+
+                        <ListGroupItem>Price: ${product.price}</ListGroupItem>
+
+                        <ListGroupItem>
+                            Description: {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Price:</Col>
+
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Status:</Col>
+
+                                    <Col>
+                                        {product.countInStock > 0
+                                            ? 'In Stock'
+                                            : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+
+                            <ListGroupItem>
+                                <Button
+                                    className='btn-block'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add to Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default ProductScreen;
